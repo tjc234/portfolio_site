c0b575990912f2456c173003704b6d04fa9c3c41
tjc234 <tjc234@nau.edu>
2024-02-23 12:38:07 -0700

    server implementation, required a lot of file restructuring

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/checkers')
+def checkers():
+    return render_template('checkers.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
